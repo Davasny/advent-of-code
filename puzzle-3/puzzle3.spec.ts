2e7a5241d0f1f869769095e1e2d19220ead4eca6
Davasny <davasny@gmail.com>
2023-12-03 20:06:35 +0100

    feat: day 3

---
+++ puzzle-3/puzzle3.spec.ts
@@ -0,0 +1,182 @@
+import { assertEquals } from "https://deno.land/std@0.205.0/assert/assert_equals.ts";
+import { puzzle3PartOne, puzzle3PartTwo } from "./puzzle3.ts";
+
+Deno.test("puzzle3", async (t) => {
+  await t.step("Test example", () => {
+    const result = puzzle3PartOne([
+      "467..114..",
+      "...*......",
+      "..35..633.",
+      "......#...",
+      "617*......",
+      ".....+.58.",
+      "..592.....",
+      "......755.",
+      "...$.*....",
+      ".664.598.."
+    ]);
+
+    assertEquals(result, 4361);
+  });
+
+  await t.step("No pattern", () => {
+    assertEquals(puzzle3PartOne([
+      "..1.....",
+      "........",
+    ]), 0)
+  })
+
+  await t.step("Line below", () => {
+    assertEquals(puzzle3PartOne([
+      "........",
+      "...1....",
+      "..*.....",
+      "........",
+    ]), 1)
+
+    assertEquals(puzzle3PartOne([
+      "........",
+      "...1....",
+      "...*....",
+      "........",
+    ]), 1)
+
+    assertEquals(puzzle3PartOne([
+      "........",
+      "...1....",
+      "....*...",
+      "........",
+    ]), 1)
+  })
+
+  await t.step("Line above", () => {
+    assertEquals(puzzle3PartOne([
+      "..*.....",
+      "...1....",
+      "........",
+      "........",
+    ]), 1)
+
+    assertEquals(puzzle3PartOne([
+      "...*....",
+      "...1....",
+      "........",
+      "........",
+    ]), 1)
+
+    assertEquals(puzzle3PartOne([
+      "........",
+      "....*...",
+      "...1....",
+      "........",
+    ]), 1)
+  })
+
+  await t.step("Same line", () => {
+    assertEquals(puzzle3PartOne([
+      "........",
+      "..*6....",
+      "........",
+    ]), 6)
+
+    assertEquals(puzzle3PartOne([
+      "........",
+      "...7*...",
+      "........",
+    ]), 7)
+  });
+
+  await t.step("Number placement", () => {
+    assertEquals(puzzle3PartOne([
+      "........",
+      ".....*11",
+      "........",
+    ]), 11)
+
+    assertEquals(puzzle3PartOne([
+      "........",
+      "22*......",
+      "........",
+    ]), 22)
+
+    assertEquals(puzzle3PartOne([
+      "........",
+      ".....*..",
+      "......33",
+    ]), 33)
+
+    assertEquals(puzzle3PartOne([
+      "44......",
+      "..*.....",
+      "........",
+    ]), 44)
+
+    assertEquals(puzzle3PartOne([
+      "......55",
+      ".....*..",
+      "........",
+    ]), 55)
+
+    assertEquals(puzzle3PartOne([
+      "........",
+      "..*.....",
+      "66......",
+    ]), 66)
+  });
+
+  await t.step("Check numbers overlapping", () => {
+    assertEquals(puzzle3PartOne([
+      "......1.",
+      "......1.",
+      "........",
+    ]), 0)
+
+  });
+
+  await t.step("Input data", async () => {
+    const data = await Deno.readTextFile("./puzzle-3/input.txt");
+    const lines = data.split("\n");
+
+    const result = await puzzle3PartOne(lines);
+
+    console.log(`Sum 3: ${result}`);
+  })
+});
+
+Deno.test("puzzle3  part two", async (t) => {
+  await t.step("Test example", () => {
+    assertEquals(
+      puzzle3PartTwo([
+        "467..114..",
+        "...*......",
+        "..35..633.",
+        "......#...",
+        "617*......",
+        ".....+.58.",
+        "..592.....",
+        "......755.",
+        "...$.*....",
+        ".664.598..",
+      ]),
+      467835,
+    );
+  });
+
+  await t.step("Test inline", () => {
+    assertEquals(
+      puzzle3PartTwo([
+        ".2.3*2..",
+      ]),
+      6,
+    );
+  });
+
+  await t.step("Input data", async () => {
+    const data = await Deno.readTextFile("./puzzle-3/input.txt");
+    const lines = data.split("\n");
+
+    const result = await puzzle3PartTwo(lines);
+
+    console.log(`Sum 3 part 2: ${result}`);
+  })
+});
---
+++ puzzle-3/puzzle3.ts
@@ -0,0 +1,175 @@
+interface IFoundNumber {
+  line: number;
+  column: number;
+  value: string;
+}
+
+const isSymbol = (char: string): boolean => {
+  if (!char) {
+    return false;
+  }
+
+  if (char === "." || char.match(/\d/)) {
+    return false;
+  }
+
+  return true;
+};
+
+const isPartNumber = (lines: string[], foundNumber: IFoundNumber): boolean => {
+  const charBeforeIsValid = isSymbol(
+    lines[foundNumber.line][foundNumber.column - 1],
+  );
+  // console.log("charBeforeIsValid", charBeforeIsValid);
+
+  const charAfterIsValid = isSymbol(
+    lines[foundNumber.line][foundNumber.column + foundNumber.value.length],
+  );
+  // console.log("charAfterIsValid", charAfterIsValid);
+
+  let anyCharAboveIsValid = false;
+  if (foundNumber.line > 0) {
+    // look at line above
+    const lineAbove = lines[foundNumber.line - 1];
+    // check also diagonal
+    const charsAbove = Array.from(
+      lineAbove.slice(
+        Math.max(0, foundNumber.column - 1),
+        Math.min(
+          lineAbove.length,
+          foundNumber.column + foundNumber.value.length + 1,
+        ),
+      ),
+    ).filter((char) => isSymbol(char));
+
+    if (charsAbove.length > 0) {
+      anyCharAboveIsValid = true;
+    }
+  }
+  // console.log("anyCharAboveIsValid", anyCharAboveIsValid);
+
+  let anyCharBelowIsValid = false;
+  if (foundNumber.line < lines.length - 1) {
+    // look at line below
+    const lineBelow = lines[foundNumber.line + 1];
+    // check also diagonal
+    const charsBelow = Array.from(
+      lineBelow.slice(
+        Math.max(0, foundNumber.column - 1),
+        Math.min(
+          lineBelow.length,
+          foundNumber.column + foundNumber.value.length + 1,
+        ),
+      ),
+    );
+
+    if (charsBelow.filter((char) => isSymbol(char)).length > 0) {
+      anyCharBelowIsValid = true;
+    }
+  }
+  // console.log("anyCharBelowIsValid", anyCharBelowIsValid);
+
+  return charBeforeIsValid || charAfterIsValid || anyCharAboveIsValid ||
+    anyCharBelowIsValid;
+};
+
+const extractNumbers = (lines: string[]): IFoundNumber[] => {
+  const numbers: IFoundNumber[] = [];
+
+  for (let lineIdx = 0; lineIdx < lines.length; lineIdx++) {
+    const line = lines[lineIdx];
+    let tmpNumber = "";
+
+    for (let charIdx = 0; charIdx < line.length; charIdx++) {
+      const char = line[charIdx];
+
+      if (char.match(/\d/)) {
+        // we have digit
+        tmpNumber = tmpNumber + char;
+      } else if (tmpNumber !== "") {
+        // save found number
+        numbers.push({
+          line: lineIdx,
+          column: charIdx - tmpNumber.length, // let's use first char position
+          value: tmpNumber,
+        });
+        tmpNumber = "";
+      }
+    }
+
+    if (tmpNumber !== "") {
+      // save found number it was last char
+      numbers.push({
+        line: lineIdx,
+        column: line.length - tmpNumber.length, // let's use first char position
+        value: tmpNumber,
+      });
+      tmpNumber = "";
+    }
+  }
+
+  return numbers;
+};
+
+export const puzzle3PartOne = (lines: string[]): number => {
+  const numbers = extractNumbers(lines);
+
+  const validNumbers = numbers.filter((number) => isPartNumber(lines, number))
+    .map((number) => parseInt(number.value));
+  // console.log(validNumbers);
+  return validNumbers.reduce((a, b) => a + b, 0);
+};
+
+export const puzzle3PartTwo = (lines: string[]): number => {
+  const numbers = extractNumbers(lines);
+  let sum = 0;
+
+  for (let lineIdx = 0; lineIdx < lines.length; lineIdx++) {
+    for (let charIdx = 0; charIdx < lines[lineIdx].length; charIdx++) {
+      const char = lines[lineIdx][charIdx];
+
+      if (char === "*") {
+        const foundNumbers = [];
+
+        const numberBefore = numbers.filter((number) =>
+          number.line === lineIdx &&
+          number.column + number.value.length === charIdx
+        ).map((number) => parseInt(number.value));
+        foundNumbers.push(...numberBefore);
+
+        const numberAfter = numbers.filter((number) =>
+          number.line === lineIdx && number.column === charIdx + 1
+        ).map((number) => parseInt(number.value));
+        foundNumbers.push(...numberAfter);
+
+        const numbersAbove = numbers
+          .filter((number) => number.line === lineIdx - 1)
+          .filter((number) =>
+            number.column + number.value.length === charIdx ||
+            number.column === charIdx + 1 ||
+            number.column === charIdx ||
+            (number.column < charIdx && charIdx < number.column + number.value.length)
+          )
+          .map((number) => parseInt(number.value));
+        foundNumbers.push(...numbersAbove);
+
+        const numbersBelow = numbers
+          .filter((number) => number.line === lineIdx + 1)
+          .filter((number) =>
+            number.column + number.value.length === charIdx ||
+            number.column === charIdx + 1 ||
+            number.column === charIdx ||
+            (number.column < charIdx && charIdx < number.column + number.value.length)
+          )
+          .map((number) => parseInt(number.value));
+        foundNumbers.push(...numbersBelow);
+
+        if (foundNumbers.length === 2) {
+          sum = sum + foundNumbers.reduce((a, b) => a * b, 1);
+        }
+      }
+    }
+  }
+
+  return sum;
+};
